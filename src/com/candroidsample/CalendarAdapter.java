8a4759928b93787f1657e9f9d2af50e58a72ac99
sanketpatel2912 <sanketp@ufony.com>
2014-12-31 13:44:12 +0530

    hh

--- src/com/candroidsample/CalendarAdapter.java
+++ src/com/candroidsample/CalendarAdapter.java
@@ -1,6 +1,5 @@
 package com.candroidsample;
 
-import hirondelle.date4j.DateTime;
 
 import java.util.HashMap;
 
@@ -43,75 +42,76 @@ public class CalendarAdapter extends CaldroidGridAdapter {
 		TextView tv2 = (TextView) cellView.findViewById(R.id.tv2);
 
 		tv1.setTextColor(Color.BLACK);
-
+		
 		// Get dateTime of this cell
-		DateTime dateTime = this.datetimeList.get(position);
-		Resources resources = context.getResources();
+//		DateTime dateTime = this.datetimeList.get(position);
+//		Resources resources = context.getResources();
 
 		// Set color of the dates in previous / next month
-		if (dateTime.getMonth() != month) {
-			tv1.setTextColor(resources
-					.getColor(com.caldroid.R.color.caldroid_darker_gray));
-		}
-
-		boolean shouldResetDiabledView = false;
-		boolean shouldResetSelectedView = false;
-
-		// Customize for disabled dates and date outside min/max dates
-		if ((minDateTime != null && dateTime.lt(minDateTime))
-				|| (maxDateTime != null && dateTime.gt(maxDateTime))
-				|| (disableDates != null && disableDates.indexOf(dateTime) != -1)) {
-
-			tv1.setTextColor(CaldroidFragment.disabledTextColor);
-			if (CaldroidFragment.disabledBackgroundDrawable == -1) {
-				cellView.setBackgroundResource(com.caldroid.R.drawable.disable_cell);
-			} else {
-				cellView.setBackgroundResource(CaldroidFragment.disabledBackgroundDrawable);
-			}
-
-			if (dateTime.equals(getToday())) {
-				cellView.setBackgroundResource(com.caldroid.R.drawable.red_border_gray_bg);
-			}
-
-		} else {
-			shouldResetDiabledView = true;
-		}
+//		if (dateTime.getMonth() != month) {
+//			tv1.setTextColor(resources
+//					.getColor(com.caldroid.R.color.caldroid_darker_gray));
+//		}
+//
+//		boolean shouldResetDiabledView = false;
+//		boolean shouldResetSelectedView = false;
+//
+//		// Customize for disabled dates and date outside min/max dates
+//		long minDateTime=null;
+//		if ((minDateTime != null && dateTime.lt(minDateTime))
+//				|| (maxDateTime != null && dateTime.gt(maxDateTime))
+//				|| (disableDates != null && disableDates.indexOf(dateTime) != -1)) {
+//
+//			tv1.setTextColor(CaldroidFragment.disabledTextColor);
+//			if (CaldroidFragment.disabledBackgroundDrawable == -1) {
+//				cellView.setBackgroundResource(com.caldroid.R.drawable.disable_cell);
+//			} else {
+//				cellView.setBackgroundResource(CaldroidFragment.disabledBackgroundDrawable);
+//			}
+//
+//			if (dateTime.equals(getToday())) {
+//				cellView.setBackgroundResource(com.caldroid.R.drawable.red_border_gray_bg);
+//			}
+//
+//		} else {
+//			shouldResetDiabledView = true;
+//		}
 
 		// Customize for selected dates
-		if (selectedDates != null && selectedDates.indexOf(dateTime) != -1) {
-			if (CaldroidFragment.selectedBackgroundDrawable != -1) {
-				cellView.setBackgroundResource(CaldroidFragment.selectedBackgroundDrawable);
-			} else {
-				cellView.setBackgroundColor(resources
-						.getColor(com.caldroid.R.color.caldroid_sky_blue));
-			}
+//		if (selectedDates != null && selectedDates.indexOf(dateTime) != -1) {
+//			if (CaldroidFragment.selectedBackgroundDrawable != -1) {
+//				cellView.setBackgroundResource(CaldroidFragment.selectedBackgroundDrawable);
+//			} else {
+//				cellView.setBackgroundColor(resources
+//						.getColor(com.caldroid.R.color.caldroid_sky_blue));
+//			}
+//
+//			tv1.setTextColor(CaldroidFragment.selectedTextColor);
+//
+//		} else {
+//			shouldResetSelectedView = true;
+//		}
 
-			tv1.setTextColor(CaldroidFragment.selectedTextColor);
-
-		} else {
-			shouldResetSelectedView = true;
-		}
-
-		if (shouldResetDiabledView && shouldResetSelectedView) {
-			// Customize for today
-			if (dateTime.equals(getToday())) {
-				cellView.setBackgroundResource(com.caldroid.R.drawable.red_border);
-			} else {
-				cellView.setBackgroundResource(com.caldroid.R.drawable.cell_bg);
-			}
-		}
-
-		tv1.setText("" + dateTime.getDay());
-		tv2.setText("Hi");
-
-		// Somehow after setBackgroundResource, the padding collapse.
-		// This is to recover the padding
-		cellView.setPadding(leftPadding, topPadding, rightPadding,
-				bottomPadding);
-
-		// Set custom color if required
-		setCustomResources(dateTime, cellView, tv1);
-
+//		if (shouldResetDiabledView && shouldResetSelectedView) {
+//			// Customize for today
+//			if (dateTime.equals(getToday())) {
+//				cellView.setBackgroundResource(com.caldroid.R.drawable.red_border);
+//			} else {
+//				cellView.setBackgroundResource(com.caldroid.R.drawable.cell_bg);
+//			}
+//		}
+//
+//		tv1.setText("" + dateTime.getDay());
+//		tv2.setText("Hi");
+//
+//		// Somehow after setBackgroundResource, the padding collapse.
+//		// This is to recover the padding
+//		cellView.setPadding(leftPadding, topPadding, rightPadding,
+//				bottomPadding);
+//
+//		// Set custom color if required
+//		setCustomResources(dateTime, cellView, tv1);
+//
 		return cellView;
 	}
 
